7fe8243b2f21591ada62b2f24ef732418edd5e6c
José Salas <josesalasbolivar98@gmail.com>
2022-10-02 10:08:33 -0500

    Project structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning model applied to failure prediction and remaining useful life estimation',
+    author='"The Datatonics"',
+    license='MIT',
+)
